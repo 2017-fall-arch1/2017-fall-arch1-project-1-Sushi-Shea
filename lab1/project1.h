b4e8b29165ebfbf23555712effb60e1f1578c353
Noa Dodson <octobersushi@yahoo.com>
2017-10-10 10:36:01 -0600

    Reconfigured to support binary tree structure

--- lab1/project1.h
+++ lab1/project1.h
@@ -5,33 +5,33 @@
 
 /*Definition of a linked list node */
 typedef struct LLNode_s {
-  struct LLNode_s *next;
+  struct LLNode_s *left, right;
   char *str;
 } LLNode;
 
 /*Structure for a list */
 typedef struct {
-  LLNode *first, *last;
-} LList;
+  LLNode *root;
+} LTree;
 
 extern llDoCheck;
 
 /* create a new list */
-LList *llAlloc();
+LTree *llAlloc();
 
 /*Free memory space for a list*/
-void llFree(LList *lp);
+void llFree(LTree *lp);
 
-/*Append a coppy of int to end of the list*/
-void llPut(LList *lp, char *s);
+/*Append a copy of char to end of the list*/
+void llPut(LTree *lp, char *s);
 
 /*Delete all elements from list*/
-void llMakeEmpty(LList *lp);
+void llMakeEmpty(LTree *lp);
 
 /*Print list membership. Prints default if message is null*/
-void llPrint(LList *lp);
+void llPrint(LTree *lp);
 
 /*Check LList consistency,always return zero*/
-int LLCheck(LList *lp, char *msg);
+int LLCheck(LTree *lp, char *msg);
 
 #endif 
